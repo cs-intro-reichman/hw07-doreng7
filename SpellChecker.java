4cb585870ac318c078c655640355cc17a914bb39
DorEng7 <dor.englender@post.runi.ac.il>
2024-02-03 19:40:49 +0200

    update

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,60 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if(str.length() == 1){
+			return "";
+		}
+		else{
+			return str.substring(1);
+		}
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if(word1.length() == 0){
+			return word2.length();
+		}	
+		if(word2.length() == 0){
+			return word1.length();
+		}	
+		if(word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
+		}		
+		else{
+			int min1 = levenshtein(tail(word1), word2);
+			int min2 = levenshtein(word1, tail(word2));
+			int min3 = levenshtein(tail(word1), tail(word2));
+			int final_min = Math.min(min3,Math.min(min2,min1));
+			return 1 + final_min;
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for(int i = 0; i < 3000; i++){
+			dictionary[i]= in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		
+		int min_index = levenshtein(word, dictionary[0]); 
+		String str_min = dictionary[0];
+		for (int i = 1; i < 3000; i++){
+			if(levenshtein(word, dictionary[i]) < min_index){
+				min_index = (levenshtein(word, dictionary[i]));
+				str_min = dictionary[i];
+			}
+		}
+		
+		if(min_index > threshold){
+			return word;
+		}
+		else{
+			return str_min;
+		}
+		
 	}
 
 }
